a47fa9129ff18a8e987a0f0c7ca96380152acad2
coder2009awesome <75286924+coder2009awesome@users.noreply.github.com>
2020-12-22 23:03:23 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,72 @@
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	helicopterIMG=loadImage("helicopter.png")
+	packageIMG=loadImage("package.png")
+}
+
+function setup() {
+	createCanvas(800, 700);
+	rectMode(CENTER);
+	
+
+	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite.addImage(packageIMG)
+	packageSprite.scale=0.2
+
+	helicopterSprite=createSprite(width/2, 200, 10,10);
+	helicopterSprite.addImage(helicopterIMG)
+	helicopterSprite.scale=0.6
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	World.add(world, packageBody);
+
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  packageSprite.x= packageBody.position.x 
+  packageSprite.y= packageBody.position.y 
+  drawSprites();
+ 
+}
+
+function keyPressed() {
+	if (keyCode === DOWN_ARROW) {
+		setTimeout(() => {packageBody.position.y= 300;}, 50);
+		setTimeout(() => {packageBody.position.y= 400;}, 100);
+		setTimeout(() => {packageBody.position.y= 500;}, 150);
+		setTimeout(() => {packageBody.position.y= 600;}, 200);
+		setTimeout(() => {packageBody.position.y= 650;}, 225);
+		setTimeout(() => {packageBody.position.y= 450;}, 100);
+		setTimeout(() => {packageBody.position.y= 650;}, 200);
+		setTimeout(() => {packageBody.position.y= 550;}, 300);
+		setTimeout(() => {packageBody.position.y= 650;}, 400);
+		setTimeout(() => {packageBody.position.y= 600;}, 500);
+		setTimeout(() => {packageBody.position.y= 650;}, 600);
+	}
+}
+
+
+
